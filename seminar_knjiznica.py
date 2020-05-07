36a1dca71507e5e0d08bbbe4ad872baa4301bf19
rusmarsel <rus.marsel@gmail.com>
2020-05-07 21:52:30 +0200

    Dela
    Tole je mnde to

--- seminar_knjiznica.py
+++ seminar_knjiznica.py
@@ -6,16 +6,16 @@ Created on Sat May  2 23:18:32 2020
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sympy import *
 
-m11=
 M0 = 1150   # [Nm]
 T = 0.17    # [s]
 w = 2*np.pi/T
-# cleni = 50
+cleni = 300
 
-a, b, t, tau, M, omega, j = symbols('a, b, t, tau, M, omega, j')
+a, b, t, tau, M, pi, j = symbols('a, b, t, tau, M, pi, j')
 
 f_mom1 = 2*M0*(t/tau)
 f_mom2 = 2*M0*(t/tau) - 2*M0
@@ -47,24 +47,23 @@ f_mom2 = 2*M0*(t/tau) - 2*M0
 # print(a_j, "Člena integrala se med seboj odštejeta. Člen je tako enak 0, lahko ga zanemarim.", "\n")
 
    
-I_1 = integrate(f_mom1*sin(j*2*np.pi/T*t), (t, a, b))
-I_2 = integrate(f_mom2*sin(j*2*np.pi/T*t), (t, a, b))
-# podatki = {a: 0, b: T/2, tau: T, M: M0}
-# podatki2 = {a: T/2, b: T, tau: T, M: M0}
-# I1_analiticno = I_1.subs(podatki).evalf() 
-# I2_analiticno = I_2.subs(podatki2).evalf()   
+I_1 = integrate(f_mom1*sin(j*t*(2*pi)/tau), (t, a, b))
+I_2 = integrate(f_mom2*sin(j*t*(2*pi)/tau), (t, a, b))
+podatki = {a: 0, b: T/2, tau: T, M: M0}
+podatki2 = {a: T/2, b: T, tau: T, M: M0}
+I1_analiticno = I_1.subs(podatki).evalf() 
+I2_analiticno = I_2.subs(podatki2).evalf()   
 # print(I1_analiticno)
 # print(I2_analiticno)
-# b_j = (2/T)*(I1_analiticno + I2_analiticno)
-bj1 = (2/T)*(I1_analiticno + I2_analiticno)
+b_j = (2/T)*(I1_analiticno + I2_analiticno)
 
 
 print(b_j)
 
 # Vzamem samo uporabne j-je, ki so med 0 in N. Ta člen nesem tudi v glavni program.
-b_j = 11.7647058823529*((-31.1147913744655*cos(3.14159265358979*j)/j + 9.90414570103852*sin(3.14159265358979*j)/j**2)+(-31.1147913744655*cos(3.14159265358979*j)/j - 9.90414570103852*sin(3.14159265358979*j)/j**2 + 9.90414570103852*sin(6.28318530717959*j)))
+# b_j = 11.7647058823529*((-31.1147913744655*cos(3.14159265358979*j)/j + 9.90414570103852*sin(3.14159265358979*j)/j**2)+(-31.1147913744655*cos(3.14159265358979*j)/j - 9.90414570103852*sin(3.14159265358979*j)/j**2 + 9.90414570103852*sin(6.28318530717959*j)))
 
-bj1 =
+
 
 # print(moment1(t_h,M0,t0))
 
@@ -84,3 +83,22 @@ bj1 =
 # Korak trapezne interpolacije
 # h = .1
 # t_h = np.linspace(0,t0,100)
+bee = np.zeros(cleni)
+for j in range(cleni):
+    if j==0:
+        bee[j]=bee[j]
+    else:
+        bee[j] = 1150*(-np.cos(j*np.pi)/(j*np.pi) + np.sin(j*np.pi)/(j**2*np.pi**2)+(-np.cos(j*np.pi)/(j*np.pi) - np.sin(j*np.pi)/(j**2*np.pi**2) + np.sin(2*j*np.pi)/(j**2*np.pi**2)))
+    
+t0 = 0
+tmax = T
+dt = .0001
+t_array = np.arange(t0,tmax,dt)
+
+M_approx = np.ones_like(t_array)
+for j in range(cleni):
+    M_approx += bee[j]*np.sin(j*w*t_array)
+    
+plt.figure()
+plt.plot(t_array, M_approx)
+plt.show()
--- seminar_python.py
+++ seminar_python.py
@@ -22,8 +22,7 @@ t0 = 1.28   # [s]
 tk = 4.22   # [s]
 
 # Fourirjeva aproksimacija funkcije momenta:
-n_max = 300  # [/]
-# t = 10;     # [s]
+n_max = 80  # [/]
 
 # Omega
 
@@ -31,15 +30,10 @@ w = 2*np.pi/T
 
 # Časovni intervali
 
-t_min = 0       # [s]
+t_min = 0.       # [s]
+t_max = T     # [s]
+dt = 0.0001     # [s]
 
-t_max = 2*T     # [s]
-dt = 0.001     # [s]
-
-dt_d = .0005     # [s]
-t_d = T      # [s]
-
-t_vec = np.arange(t_min, t_d, dt_d)
 t_array = np.arange(t_min, t_max, dt)
 t_impulz = np.arange(t_min, t0, dt)
 
@@ -223,22 +217,6 @@ np.set_printoptions(suppress=True)
 # print(np.around(M_modalna,decimals=2))
 # print(np.around(K_modalna,decimals=2))
 
-
-def M_vzb_modalna(N):
-   '''Izračuna matriko modalnih vrednosti momenta'''
-   M_vzb = []
-   for i in range(N):
-       if i < 2:
-           M_vzb.append([1])
-       else:
-           M_vzb.append([0])
-   return np.matmul(np.transpose(fi_last), M_vzb)
-    
-# print(M_vzb_en(N))
-
-# M_vzb_mod = (np.matmul(np.transpose(fi_last), M_vzb_en(N)))
-# print(M_vzb_modalna(N))
-
 # =============================================================================
 # Izračun integralske enačbe (odziv sistema)
 # =============================================================================
@@ -259,7 +237,7 @@ def koef_bj(M0, n_max):
             # Ker je vrednost a_j zelo majhna (skoraj nična), sem ji pripisal vrednost nič (razlog je lihost funkcije M2).
             # a_j[j] = 11.7647058823529*Piecewise((-5.6843418860808e-14, Eq(j, 0)), (31.1147913744655*sin(3.14159265358979*j)/j + 31.1147913744655*sin(9.42477796076938*j)/j - 9.90414570103852*cos(3.14159265358979*j)/j**2 + 9.90414570103852*cos(9.42477796076938*j)/j**2, True))
             a_j[j] = 0
-            b_j[j] = 11.7647058823529*((-31.1147913744655*np.cos(3.14159265358979*j)/j + 9.90414570103852*np.sin(3.14159265358979*j)/j**2)+(-31.1147913744655*np.cos(3.14159265358979*j)/j - 9.90414570103852*np.sin(3.14159265358979*j)/j**2 + 9.90414570103852*np.sin(6.28318530717959*j)))
+            b_j[j] = 2*M0*(-np.cos(j*np.pi)/(j*np.pi))
             
     return a_j, b_j
 
@@ -284,8 +262,7 @@ def primerjava(t, moment2, a_j, b_j, w, show = False):
         plt.legend(loc="upper right")
         plt.show()
 
-    
-    
+        
 # def mom_four(t, b_j):
 #     M_four = np.ones_like(t_array) 
         
@@ -293,491 +270,172 @@ def primerjava(t, moment2, a_j, b_j, w, show = False):
 
 
 def mom_four(t_interval, b_j, show=False):
+    '''Funkcija izračuna vrednosti momenta za dani časovni interval'''
     M_four = np.ones_like(t_interval) 
-
-
     for j in range(len(b_j+1)):
         if j==0:
             M_four *= 0
         else:
-
-            M_four += b_j[j]*np.sin(j*w*t_interval)
-            
+            M_four += b_j[j]*np.sin(j*w*t_interval)           
     if show:
         plt.figure()
         plt.plot(t_interval, M_four)
-        plt.show()
-
-    
+        plt.show()    
     return M_four
-   
-# Eksaktna funkcija vzbujevalnega momenta
+
+
+# Eksaktna funkcija vzbujevalnega momenta (za preverjanje, koda iz vaj)
 M_exact = moment2(t_array, T, M0, show = False)  # Za prikaz -> show = True
-
 a_j, b_j = koef_bj(M0, n_max)
-
 primerjava(t_array, M_exact, a_j, b_j, w, show = False)  # Za prikaz -> show = True
 
 # =============================================================================
 # Modalna obremenitev
 # =============================================================================
+# To matriko uporabim kot zapis faktorjev pri prehodu v modalne koordinate
+def M_vzb_modalna(N):
+   '''Izračuna matriko modalnih koeficientov momenta'''
+   M_vzb = np.zeros((N,1))
+   for i in range(N):
+       if i < 2:
+           M_vzb[i,0] = 1
+   M_vzb = np.matmul(np.transpose(fi_last), M_vzb)    # fi_last - matrika lastnih vektorjev
+   M_vzb = np.matmul(np.linalg.inv(M_modalna), M_vzb) # M_modalna - matrika modalnih mas
+   return M_vzb
 
-obremenitev = np.zeros((N,1)) 
-obremenitev[0,0] = 1
-obremenitev[1,0] = 1
+
+m_breme = M_vzb_modalna(N)
 
 
-# plt.figure()
-# plt.plot(t_array, mom_four(t_array, b_j))
+# def b_j_modalni(P_S, b_j):
+#     h_j=np.zeros(n_max)
+#     for j in range(n_max):
+#         if j==0:
+#             h_j = h_j
+#         else:
+#             h_j[j] = b_j[j]*m_breme[P_S,0]/las_vr[P_S]   # bj/woi^2
+#     return h_j
 
-# def beta(N, n_max):
-        
-#     Beta = np.ones(N)
+
+def b_js(t):
+    moment = mom_four(t, b_j)    
+    four = np.ones((N,len(t)))
     
-#     for i in range(N):
-#         for j in range(n_max):
-            
-#             Beta[i] += 1/(1-((j*w)/las_frek[i])**2)
+    # sinus = np.ones_like(t)    
+    # if (j*w) < las_frek[P_S]: # primerja, dokler je j*w < w0i
+    #         sinus += np.sin(j*w*t_int)
+    #     else:
+    #         sinus += np.sin(j*w*t_int - np.pi)  
+    
+    for i in range(len(t)):
+        for p in range(N):
+            for j in range(n_max):
+                    if (j*w) < las_frek[p]: # primerja, dokler je j*w < w0i
+                        four[p,i] = (np.sin(j*w*t[i])*(moment[i]*m_breme[p])*(1/(1-((j*w)/las_frek[p])**2)))/las_vr[p]
+                    else:
+                        four[p,i] = (np.sin(j*w*t[i]-np.pi)*(moment[i]*m_breme[p])*(1/(1-((j*w)/las_frek[p])**2)))/las_vr[p]         
         
+    # hij = b_j_modalni(P_S, b_j)
+    # sinus = np.ones_like(t)
+    
+    # for i in range(len(t)):
+    #     for j in range(n_max):
+    #         if j == 0:
+    #             hij=hij
+    #         else:
+    #             sinus[i] += hij[j]*np.sin(j*w*t[i])
+    return four
+
+# hjs = b_js(t_array,0)
+# plt.plot(t_array, hjs)
+
+# bj = np.ones((N,len(t_array)))    
+# for j in range(N):
+#     bj[j,:] = b_js(t_array, j)
+# def breme (t_int, P_S):
+#     '''Izračuna obremenitev v časovnem intervalu za vsako prostostno stopnjo posebej'''
+#     mom_fourier = mom_four(t_int, b_j)
+#     hi_modalna = np.ones((P_S,len(t_int)))    
+#     for i in range(len(t_int)):        
+#         br = m_breme * mom_fourier[i]    
+#         hi_t_mod = np.dot(np.transpose(fi_last), br)            
+#         for j in range(P_S):            
+#             hi_modalna[j,i] = hi_t_mod[j]*(1/M_modalna[j,j])       #     np.dot(np.linalg.inv(MasMx), hi_t_mod)[j]  
+#     return hi_modalna
+
+
+# def h_i_t(t_interval, b_j, P_S, show = False):
+#     '''Izračun vektorja momentov v modalnih koordinatah - IZRAČUNANO LE NA PROSTOSTNI STOPNJI P_S (med 0 in N-1)'''    
+#     br = breme(t_interval, N)    
+#     hi_mi = np.ones(len(t_interval))    
+#     for i in range(len(t_interval)):
+#         hi_mi[i] = br[P_S,i]        
+#     if show:
+#         plt.figure()
+#         plt.plot(t_interval, hi_mi)
+#         plt.show()        
+#     return hi_mi
+# # h_i_t(t_array, b_j, 0, show=True)
+
+
+# def beta_j(P_S):
+#     '''Izračun faktorja beta na prostostni stopnji'''
+#     Beta=0    
+#     for j in range(n_max):
+#         Beta += 1/(1-((j*w)/las_frek[P_S])**2)     # las_frek nosi vrednosti w0i   
 #     return Beta
 
 
-# def X_3(N, b_j, n_max):
-    
-#     X = np.ones(N) 
-    
-#     for i in range(N):
-#         for j in range(n_max):            
-#             X[i] += b_j[j] / (MasMx[i,i]*(las_frek[i])**2)
-    
-#     return X
-
-
-# def odziv(t, n_max, N, X_3, beta):
-    
-#     Beta = beta(N,n_max)
-#     X3 = X_3(N, b_j,n_max)
-    
-#     odz = np.ones_like(t_array) 
-    
-#     for i in range(N):
-#         for j in range(n_max):
-#             odz += np.sin(j*w*t)*Beta[i]*X3[i]*np.sin(j*w*t)
-    
-#         # plt.figure()
-#         # plt.plot(t, odz)
-#         # plt.show()
-#     return odz
-        
-# odziv(t_array, n_max, N, X_3, beta)
-
-# Beta = beta(N,n_max)
-# X3 = X_3(N, b_j,n_max)
-# print(Beta[0]*X3[0]*np.sin(1*w*t_array[1]))
-
-# X = X_3(N, b_j, n_max)
-# beta_1 = beta(N, n_max)
-
-# print(X[0]*beta_1[0])
-
-# def Odziv(N, n_max, moment2, beta):
-    
-#     Od = np.ones((len(t_array), N))
-    
-#     for i in range(N):
-#         for j in range(n_max):
-            
-#             Od[i] += beta 
-        
-#     return Od
-
-
-
-# def g_t(t, M_vzb_modalna, mom_four):
-#     M_x = mom_four(t, b_j)
-#     mod_m = M_vzb_modalna(N)
-#     return mod_m * M_x[n_max]
-
-# print(g_t(0.1,M_vzb_modalna, mom_four))
-
-
-# def eta(t, N):
-    
-#     phi_i = np.ones(N)
-    
-#     for i in range(N):
-#         G = g_t(t, M_vzb_modalna, mom_four)[i,0]
-#         B = Beta[i]
-#         OM = las_vr[i]
-#         masa = M_modalna[i,i]
-#         # print(B)
-        
-#         phi_i[i] = (180/np.pi)*np.sin(w*t)*(((G/masa) * B)/OM)
-
-#     return phi_i
-    
-# Eta = eta(t_array, N)
-# print(t)
-# print(fi_last, "\n")
-# print(Eta, "\n")
-# print(np.matmul(fi_last, Eta))
-       
-# plt.figure()
-# plt.plot(t_array, phi_i)
-
-
-def breme (t_int, P_S):
-    '''Izračuna obremenitev v časovnem intervalu za vsako prostostno stopnjo'''
-    mom_fourier = mom_four(t_int, b_j)
-
-    hi_modalna = np.ones((P_S,len(t_int)))
-    
-    for i in range(len(t_int)):
-        
-        br = obremenitev * mom_fourier[i]
-    
-        hi_t_mod = np.dot(np.transpose(fi_last), br)
-            
-        for j in range(P_S):
-            
-            hi_modalna[j,i] = np.dot(np.linalg.inv(M_modalna), hi_t_mod)[j]
-            
-    # for x in range(P_S):           
-    #     plt.figure()
-    #     plt.plot(t_int, hi_modalna[x,:])
-    #     plt.show()
-        
-    return hi_modalna
-
-
-def h_i_t(t_interval, b_j, P_S, show = False):
-    '''Izračun vektorja momentov v modalnih koordinatah - IZRAČUNANO LE NA PROSTOSTNI STOPNJI P_S (med 0 in N-1)'''
-    
-    br = breme(t_interval, N)
-    
-    hi_mi = np.ones(len(t_interval))
-    
-    for i in range(len(t_interval)):
-        hi_mi[i] = br[P_S,i]
-
-        
-    if show:
-        plt.figure()
-        plt.plot(t_interval, hi_mi)
-        plt.show()
-        
-    return hi_mi
-
-# h_i_t(t_array, b_j, 0, show=True)
-
-def beta_j(P_S):
-    Beta=0    
-    for j in range(n_max):
-        Beta += 1/(1-((j*w)/las_frek[P_S])**2)
-        
-    return Beta
-
-
 # def sin_fi_t(t_int, P_S, show=False):
-#     sinus = np.ones_like(t_int)
-#     for i in range(len(t_int)):      
-#         for j in range(n_max):
-#             if (j*w) < las_frek[P_S]:
-#                 sinus[i] += np.sin(j*w*t_int[i]-0)
-#             else:
-#                 sinus[i] += np.sin(j*w*t_int[i]-np.pi)
+#     '''Izračun sinusa sin(jwt-fi) v časovnem intervalu za eno prostostno stopnjo'''
+#     sinus = np.ones_like(t_int)    
+#     for j in range(len(b_j+1)):  
+#         if (j*w) < las_frek[P_S]: # primerja, dokler je j*w < w0i
+#             sinus += np.sin(j*w*t_int)
+#         else:
+#             sinus += np.sin(j*w*t_int - np.pi)            
 #     if show:
 #         plt.figure()
-#         plt.plot(t_int,sinus)
-#         plt.show()            
+#         plt.plot(t_int, sinus)
+#         plt.show()    
 #     return sinus
 
 
-def sin_fi_t(t_int, P_S, show=False):
-    sinus = np.ones_like(t_int)
-    
-    for j in range(len(b_j+1)):
-        if (j*w) < las_frek[P_S]:
-            sinus += np.sin(j*w*t_int)
-        else:
-            sinus += np.sin(j*w*t_int - np.pi)
-            
-    if show:
-        plt.figure()
-        plt.plot(t_int, sinus)
-        plt.show()
-    
-    return sinus
+# def b_jt(t, P_S):
+#     bj = np.ones_like(t)
+#     for i in range(n_max):
+#         bj += b_j[i]*np.sin(j*w*t)*m_breme[P_S]       
+#     return bj
 
 
-mo = np.zeros((N,1))
-mo[0,0] = 1
-mo[1,0] = 1
-
-m_mo = np.dot(np.transpose(fi_last), mo)
-# print(m_mo)
-m_mo = np.dot(np.linalg.inv(MasMx),m_mo)
-    
-def b_jt(t, P_S):
-    bj = np.ones_like(t)
-    for i in range(n_max):
-        bj += b_j[i]*np.sin(j*w*t)*m_mo[P_S]
-
-
-    bj = bj/las_vr[P_S]        
-    return bj
-# bhh = b_jt(t_array, 0)
-# sin_fi_t(t_array, 0, show=True)
-
-# print(beta_j(5))
-
-def eta_t(t, beta_j, sin_fi_t, h_i_t):
-    
-    eta = np.ones((N,len(t)))
-
-            
-    for j in range(N):
+# def eta_t(t, P_S):
+#     '''Izračun vektorja eta za vse prostostne stopnje v časovnem intervalu t'''    
+#     # eta = np.ones((N,len(t)))
+#     eta = b_js(t)            
+#     for j in range(P_S):        
+#         # Be = beta_j(j)
+#         # sinus = sin_fi_t(t, j)
+#         # hi = h_i_t(t, b_j, j)
         
-        bj = b_jt(t, j)
-        Be = beta_j(j)
-        sinus = sin_fi_t(t, j)
-        # hi = h_i_t(t, b_j, j)
+#         # for i in range(len(t)):
+#         #     eta[j, i] = Be*sinus[i]*bj[j,i] # las_vrvzame vrednost w0i^2
         
-        for i in range(len(t)):
-            eta[j, i] = Be*sinus[i]*bj[i]
-        
-        # plt.figure()
-        # plt.plot(t,eta[j,:])
-        # plt.show()
-    return eta
+#         # plt.figure()
+#         # plt.plot(t,eta[j,:])
+#         # plt.show()
+#       return eta
 
-
-
-# b_j[j]*Beta[i]
-
-
-# plt.figure()
-# plt.plot(t_array, mom_four(t_array, b_j, w))
-
-# ajx  = eta_t(t_vec, beta_j, sin_fi_t, h_i_t)
-
-x_t = np.ones((N,len(t_vec)))
-Vec_Eta = eta_t(t_vec, beta_j, sin_fi_t, h_i_t)
-
-# print(Vec_Eta[:, 2])
+x_t = np.ones((N,len(t_array)))
+Vec_Eta = b_js(t_array)
 
 for i in range(N):
-    for j in range(len(t_vec)):
+    for j in range(len(t_array)):
         eta = Vec_Eta[:,j]
         
-        x_t[:,j] = np.dot(fi_last, eta)
-        
+        x_t[:,j] = np.dot(fi_last, eta)        
 
-for i in range(N):        
-    plt.figure()
-    plt.plot(t_vec, (180/np.pi)*x_t[i,:])
-    plt.xlabel('Čas [s]')
-    plt.ylabel('Zasuk [°]')
-    plt.show()
-
-# =============================================================================
-# =============================================================================
-# # 
-# =============================================================================
-# =============================================================================
-
-# def m_f_trenutni(t, b_j):    
-#     for j in range(len(b_j+1)):
-#         if j==0:
-#             M_four = 0
-#         else:
-#             M_four += b_j[j]*np.sin(j*w*t)
-#     # print(M_four)
-#     return M_four
-
-
-# # m_f_trenutni(0.083, b_j)
-
-
-    
-# def g_t(fi_last, mom_four):
-#     np.transpose(fi_last)
-
-
-# def h_tren(t, b_j, P_S):
-#     '''Izračun vrednosti momenta pri času t'''    
-#     mom_fourier = m_f_trenutni(t, b_j)    
-#     hi_mi = hi_t_mod[P_S,0]*mom_fourier    
-#     # print(hi_mi)
-#     return hi_mi
-
-
-# # h_tren(0.083, b_j, 5)
-
-
-# def sin_fi(t, P_S, show=False):
-#     sinus = 0       
-#     for j in range(n_max):
-#         if (j*w) < las_frek[P_S]:
-#             sinus += np.sin(j*w*t-0)
-#         else:
-#             sinus += np.sin(j*w*t-np.pi)
-                
-#     return sinus
-
-# def eta_t(t, beta, sin_fi, h_tren, P_S):
-    
-#     eta = np.ones_like(t)
-    
-#     for i in range(len(t)):
-#         eta[i] = (1/las_vr[P_S])*h_tren(t[i], b_j, P_S)*beta_j(N, P_S)*sin_fi(t[i], P_S)
-        
-#     plt.figure()
-#     plt.plot(t_array,eta)
-#     plt.show()
-#     return eta
-
-# def eta_vec(t, beta, sin_fi, h_tren):
-#     vec_eta = np.ones((N,1))    
-#     for i in range(N):
-#         vec_eta[i,0] = (1/las_vr[i])*h_tren(t, b_j, i)*beta_j(N, i)*sin_fi(t, i)
-#     # print(vec_eta)    
-#     return vec_eta
-
-
-# odz = np.ones((N,len(t_vec)))
-
-# =============================================================================
-# =============================================================================
-# # 
-# =============================================================================
-# =============================================================================
-
-
-# for i in range(1):
-#     for j in range(len(t_vec)):
-#         ETA = eta_vec(t_vec[j], beta_j, sin_fi, h_tren)   
-#         x_t = np.dot(fi_last, ETA)
-#         odz[i,j] = x_t[i]
-
-# plt.figure()
-# plt.plot(t_vec, (180/np.pi)*odz[0,:])
-# plt.show    
-
-# def obremenitev(m_four):
-
-# def H_3(N, b_j, n_max):
-    
-#     H = np.ones(N) 
-    
-#     for i in range(N):
-#         for j in range(n_max):            
-#             H[i] += b_j[j] / (MasMx[i,i]*(las_frek[i])**2)
-    
-#     return H
-
-
-# def odziv(t, n_max, N, X_3, beta):
-    
-#     Beta = beta(N,n_max)
-#     X3 = X_3(N, b_j,n_max)
-    
-#     odz = np.ones_like(t_array) 
-    
-#     for i in range(N):
-#         for j in range(n_max):
-#             odz += np.sin(j*w*t)*Beta[i]*X3[i]*np.sin(j*w*t)
-    
-#         plt.figure()
-#         plt.plot(t, odz)
-#         plt.show()
-#     return odz
-        
-# odziv(t_array, n_max, N, X_3, beta)
-
-# Beta = beta(N,n_max)
-# X3 = X_3(N, b_j,n_max)
-# print(Beta[0]*X3[0]*np.sin(1*w*t_array[1]))
-
-# X = X_3(N, b_j, n_max)
-# beta_1 = beta(N, n_max)
-
-# print(X[0]*beta_1[0])
-
-# def Odziv(N, n_max, moment2, beta):
-    
-#     Od = np.ones((len(t_array), N))
-    
-#     for i in range(N):
-#         for j in range(n_max):
-            
-#             Od[i] += beta 
-        
-#     return Od
-
-
-
-# def g_t(t, M_vzb_modalna, mom_four):
-#     M_x = mom_four(t, b_j)
-#     mod_m = M_vzb_modalna(N)
-#     return mod_m * M_x[n_max]
-
-# print(g_t(0.1,M_vzb_modalna, mom_four))
-
-
-# def eta(t, N):
-    
-#     phi_i = np.ones(N)
-    
-#     for i in range(N):
-#         G = g_t(t, M_vzb_modalna, mom_four)[i,0]
-#         B = Beta[i]
-#         OM = las_vr[i]
-#         masa = M_modalna[i,i]
-#         # print(B)
-        
-#         phi_i[i] = (180/np.pi)*np.sin(w*t)*(((G/masa) * B)/OM)
-
-#     return phi_i
-    
-# Eta = eta(t_array, N)
-# print(t)
-# print(fi_last, "\n")
-# print(Eta, "\n")
-# print(np.matmul(fi_last, Eta))
-       
-# plt.figure()
-# plt.plot(t_array, phi_i)
-            
-
-
-
-# b_j[j]*Beta[i]
-
-
-# plt.figure()
-# plt.plot(t_array, mom_four(t_array, b_j, w))
-
-
-    
-# def g_t(fi_last, mom_four):
-#     np.transpose(fi_last)
-
-# def vectorfield(t, y, N, a_j, b_j, M_modalna, K_modalna):
-    
-# Vectorfield function takes a state space vector as input and returns its derivative
-# at given time instance. Arbitrary arguments can be passed for the derivative calculation
-#
-# inputs
-# y:        state-space vector; in the current case of prism and cylinder it consists of
-#           y = [y[0], y[1], y[2], y[3]] = [x, dx/dt, s, ds/dt] 
-# t:        time instance; this is automatically provided by odeint function
-# params:   a tuple of additional arguments
-#
-# outputs
-# dy:       a derivative of a state-space vector; in the current case it consists of
-#           dy = [dx/dt, d^2x/dt^2, ds/dt, d^2s/dt^2]
-
-    
-    #define matrix A according to devised equilibrium eqs
-
+plt.figure()
+plt.plot(t_array, (180/np.pi)*x_t[0,:])
+plt.xlabel('Čas [s]')
+plt.ylabel('Zasuk [°]')
+plt.show()
